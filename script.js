db0f520665ed1b528cd71844d23d1010e509171a
SAI TEJA H R <98202897+Saitejahr@users.noreply.github.com>
2023-02-26 18:49:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,2 @@
-let yourArray=['one', 2, null, true,undefined];
+let yourArray = ['str', 1, true, undefined, null];
 console.log(yourArray.length);
-
-// let yourArray = ['str', 1, true, undefined, null];
-// console.log(yourArray.length);
